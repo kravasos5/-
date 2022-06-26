c1cf68b2f34fc735b330dfe0a9b8ece0e3475118
PuДJe_EnJoy3R <92521154+kravasos5@users.noreply.github.com>
2022-06-26 12:41:20 +0500

    Delete start.bat

--- energy/start.bat
+++
@@ -1,4 +0,0 @@
-python K:\hacks\parser\parser_v1\akkermann\csv_creator.py
-python K:\hacks\parser\parser_v1\akkermann\count_creator.py
-python K:\hacks\parser\parser_v1\akkermann\parser_v1.py
-pause
